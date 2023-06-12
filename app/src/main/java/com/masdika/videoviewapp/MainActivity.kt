c43bc4e3ef88486e0837e55f12238caf24cf63b7
Masdikaa <masdikailhanmansiz@gmail.com>
2023-06-12 20:15:47 +0700

    Generate views for Video Viewa

--- app/src/main/java/com/masdika/videoviewapp/MainActivity.kt
+++ app/src/main/java/com/masdika/videoviewapp/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.masdika.videoviewapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import com.masdika.videoviewapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var myVideoView: VideoView? = null
+    private var mediaController: MediaController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
