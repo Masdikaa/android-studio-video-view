3a3db7208701bab1ee0f67542bdd997af1410945
Masdikaa <masdikailhanmansiz@gmail.com>
2023-06-12 20:43:31 +0700

    Executing media raw

--- app/src/main/java/com/masdika/videoviewapp/MainActivity.kt
+++ app/src/main/java/com/masdika/videoviewapp/MainActivity.kt
@@ -1,19 +1,55 @@
 package com.masdika.videoviewapp
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.MediaController
-import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.masdika.videoviewapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var myVideoView: VideoView? = null
     private var mediaController: MediaController? = null
+    private val videoUrl: String = "https://youtu.be/IjN3FWoXHjQ"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlay.setOnClickListener {
+
+            mediaController = MediaController(this)
+            mediaController!!.setAnchorView(binding.videoView)
+            val onlineUrl = Uri.parse("android.resource://$packageName/${R.raw.sample}")
+
+            binding.videoView.setMediaController(mediaController)
+            binding.videoView.setVideoURI(onlineUrl)
+            binding.videoView.requestFocus()
+            binding.videoView.start()
+
+        }
+
     }
+
+//    private fun playVideo() {
+//        if (mediaController == null) {
+//            mediaController = MediaController(this)
+//            mediaController!!.setAnchorView(this.binding.videoView)
+//        }
+//        binding.videoView!!.setMediaController(mediaController)
+//        binding.videoView!!.setVideoURI(Uri.parse(videoUrl))
+//
+//        binding.videoView!!.requestFocus()
+//        binding.videoView!!.pause()
+//        binding.videoView!!.setOnCompletionListener {
+//            Toast.makeText(this, "Video Selesai", Toast.LENGTH_SHORT).show()
+//        }
+//        binding.videoView!!.setOnErrorListener { mediaPlayer, i, i2 ->
+//            Toast.makeText(applicationContext, "Eror", Toast.LENGTH_SHORT).show()
+//            false
+//        }
+//    }
 }
